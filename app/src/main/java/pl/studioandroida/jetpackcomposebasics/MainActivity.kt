d205e6b9a7afeb81810d92960f14d0e504baf2b7
Filip Palusiński <palusinskifilip@gmail.com>
2021-09-25 15:58:06 +0200

    Completed the 2. and 3. step

--- app/src/main/java/pl/studioandroida/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/pl/studioandroida/jetpackcomposebasics/MainActivity.kt
@@ -3,36 +3,49 @@ package pl.studioandroida.jetpackcomposebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import pl.studioandroida.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeBasicsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            MyApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(){
+    JetpackComposeBasicsTheme {
+        androidx.compose.material.Surface(color = Color.Yellow) {
+            Greeting("Android")
+        }
+
+    }
 }
+
+@Composable
+fun Greeting(name: String) {
+        Text(
+            text = "Hello $name!",
+            modifier = Modifier.padding(24.dp)
+        )
+}
+
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    JetpackComposeBasicsTheme {
-        Greeting("Android")
-    }
+    MyApp()
 }
