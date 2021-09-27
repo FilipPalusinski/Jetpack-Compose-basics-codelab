952b92795b4fc54e9706ffb0776e8e9300432bb6
Filip Palusiński <palusinskifilip@gmail.com>
2021-09-27 14:26:31 +0200

    Completed 6th step

--- app/src/main/java/pl/studioandroida/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/pl/studioandroida/jetpackcomposebasics/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import pl.studioandroida.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
@@ -34,16 +38,21 @@ fun MyApp(content: @Composable () -> Unit){
         }
     }
 }
-
 @Composable
-fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
-    val counterState = remember {mutableStateOf(0)}
-    Column {
-        for(name in names) {
+fun NameList(names: List<String>, modifier: Modifier) {
+    LazyColumn(modifier = modifier) {
+        items (items = names) { name ->
             Greeting(name = name)
             Divider(color = Color.Black)
         }
-        Divider(color = Color.Transparent, thickness = 32.dp)
+    }
+}
+
+@Composable
+fun MyScreenContent(names: List<String> = List(1000) {"Hello Android #$it"}) {
+    val counterState = remember {mutableStateOf(0)}
+    Column(modifier = Modifier.fillMaxHeight()) {
+        NameList(names, Modifier.weight(1f) )
         Counter(
             count = counterState.value,
             updateCount = { newCount ->
@@ -65,7 +74,12 @@ fun Greeting(name: String) {
 
 @Composable
 fun Counter(count: Int, updateCount: (Int) -> Unit) {
-    Button(onClick = { updateCount(count+1) }) {
+    Button(
+        onClick = { updateCount(count+1) },
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = if (count > 5) Color.Green else Color.White
+        )
+    ) {
         Text("I've been clicked $count times")
     }
 }
