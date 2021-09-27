1c747a2035844b0489f2e59bb6c2950ce65e22f2
Filip Palusiński <palusinskifilip@gmail.com>
2021-09-27 14:12:34 +0200

    Completed 5th step

--- app/src/main/java/pl/studioandroida/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/pl/studioandroida/jetpackcomposebasics/MainActivity.kt
@@ -5,11 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,14 +37,23 @@ fun MyApp(content: @Composable () -> Unit){
 
 @Composable
 fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+    val counterState = remember {mutableStateOf(0)}
     Column {
         for(name in names) {
             Greeting(name = name)
             Divider(color = Color.Black)
         }
+        Divider(color = Color.Transparent, thickness = 32.dp)
+        Counter(
+            count = counterState.value,
+            updateCount = { newCount ->
+                counterState.value = newCount
+            }
+        )
 
     }
 }
+
 
 @Composable
 fun Greeting(name: String) {
@@ -55,7 +63,12 @@ fun Greeting(name: String) {
         )
 }
 
-
+@Composable
+fun Counter(count: Int, updateCount: (Int) -> Unit) {
+    Button(onClick = { updateCount(count+1) }) {
+        Text("I've been clicked $count times")
+    }
+}
 
 
 @Preview(showBackground = true)
